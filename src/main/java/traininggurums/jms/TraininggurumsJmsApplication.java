329a7d5ef9bd6e34eb042391bba511ae6ee41689
lizersaiz <lizer.saiz.ads@gmail.com>
2021-02-03 12:09:24 +0100

    initial commit

---
+++ src/main/java/traininggurums/jms/TraininggurumsJmsApplication.java
@@ -0,0 +1,13 @@
+package traininggurums.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraininggurumsJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraininggurumsJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/traininggurums/jms/TraininggurumsJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package traininggurums.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraininggurumsJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
